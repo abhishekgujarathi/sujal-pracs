2da6f46d199441184f433cf1fead58058cf76a59
joker45op <abhiguj13579@gmail.com>
2026-05-24 21:54:48 +0530

    Prac 21

---
+++ Practical_21/Prac/Practical_21/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_21.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                Content = "<h1>Hello from published site</h1>",
+                ContentType = "text/html"
+            };
+        }
+    }
+}
